735b87a6adce765449b57139b7473084a8020ec9
dinhhainam301098 <58589521+dinhhainam301098@users.noreply.github.com>
2020-07-04 10:16:44 +0700

    Add files via upload

--- Hai_Nam/AlphaCoronaVirus.cpp
+++ Hai_Nam/AlphaCoronaVirus.cpp
@@ -25,22 +25,21 @@ AlphaCoronaVirus::AlphaCoronaVirus(const AlphaCoronaVirus& obj)
 void AlphaCoronaVirus::doBorn()
 {
 	loadADNInformation();
-	int a = 0;
-	int b = 1;
 	m_color = randFunction(0, 1) ? Color::BLUE : Color::RED;
 	// log("AlphaCoronaVirus doBorn() \n");
 
 }
 
-std::vector< CoronaVirus*> AlphaCoronaVirus::doClone()
+std::list< CoronaVirus*> AlphaCoronaVirus::doClone()
 {
-	vector<CoronaVirus*> listVirus;
+	list<CoronaVirus*> listVirus;
 	AlphaCoronaVirus* newAlphaVirus = new AlphaCoronaVirus();
 	newAlphaVirus->setDNA(m_dna);
 	newAlphaVirus->setVirusResistance(m_virusResistance);
 	newAlphaVirus->setColor(m_color);
 	listVirus.push_back(newAlphaVirus);
 	// log("AlphaCoronaVirus doClone() \n");
+	
 
 	return listVirus;
 
@@ -54,34 +53,15 @@ void AlphaCoronaVirus::doDie()
 
 void AlphaCoronaVirus::initResistance()
 {
-	//if (m_color == Color::RED)
-	//{
-	//    int a = 10;
-	//    int b = 20;
-	//    setVirusResistance(randFunction(a, b)); // virusResistance = rand(10-20)
-	//}
-	//else if (m_color == Color::BLUE)
-	//{
-	//    int a = 10;
-	//    int b = 15;
-	//    setVirusResistance(randFunction(a, b)); // virusResistance = rand(10-15)
-	//}
-	//int a = 10;
-	//int b = 20;
-	//m_virusResistance = Color::RED ? randFunction(10, 20) : randFunction(10, 15);
-	switch (m_color)
+	if (m_color == Color::RED)
 	{
-	case RED: {
-		this->m_virusResistance = randFunction(10, 20);
-		break;
+	    setVirusResistance(randFunction(10, 20)); // virusResistance = rand(10-20)
 	}
-	case BLUE:
+	else if (m_color == Color::BLUE)
 	{
-		this->m_virusResistance = randFunction(10, 15);
-		break;
+	    setVirusResistance(randFunction(10, 15)); // virusResistance = rand(10-15)
 	}
-	}
-
+	
 	//  log("AlphaCoronaVirus initResistance() \n");
 }
 
