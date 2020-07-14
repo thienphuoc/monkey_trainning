72fd29fc0efe668975b7bebe42b9ab58085587ef
dinhhainam301098 <hainamm70@gmail.com>
2020-07-14 08:22:35 +0700

    a

---
+++ Hai_Nam/ttt2/Classes/Definitions.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#define SCENE_TRANSITION_TIME 1.0f
+#define SPLASH_SCENE_SHOW_TIME 3.0f
+
+
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "Image/Splash Background.png"
+#define MAIN_MENU_BACKGROUND_FILEPATH "Image/Main Menu Background.png"
+#define GAME_BACKGROUND_FILEPATH "Image/Main Menu Background.png"
+#define GAME_OVER_BACKGROUND "Image/Pause Background.png"
+#define PAUSE_BACKGROUND "Image/Pause Background.png"
+
+#define GAME_OVER_WINDOW "Image/Pause Window.png"
+#define PAUSE_WINDOW "Image/Pause Window.png"
+
+#define MAIN_MENU_TITLE_FILEPATH "Image/Game Title.png"
+
+#define MAIN_MENU_PLAY_BUTTON "Image/Play Button.png"
+#define MAIN_MENU_PLAY_BUTTON_PRESSED "Image/Play Button.png"
+#define SOUND_ON_BUTTON "Image/Sound On.png"
+#define SOUND_ON_BUTTON_PRESSED "Image/Sound On.png"
+#define SOUND_OFF_BUTTON "Image/Sound Off.png"
+#define SOUND_OFF_BUTTON_PRESSED "Image/Sound Off.png"
+#define ACHIEVEMENTS_BUTTON "Image/Achievements.png"
+#define ACHIEVEMENTS_BUTTON_PRESSED "Image/Achievements.png"
+#define RETRY_BUTTON "Image/Retry Button.png"
+#define RETRY_BUTTON_PRESSED "Image/Retry Button.png"
+#define HOME_BUTTON "Image/Home Button.png"
+#define HOME_BUTTON_PRESSED "Image/Home Button.png"
+#define PAUSE_BUTTON "Image/Pause Button.png"
+#define PAUSE_BUTTON_PRESSED "Image/Pause Button.png"
+#define RESUME_BUTTON "Image/Resume Button.png"
+#define RESUME_BUTTON_PRESSED "Image/Resume Button.png"
+
+#define MAIN_MENU_PLAY_BUTTON_OUTER "Image/Play Button Outer.png"
+
+#define GRID_FILEPATH "Image/Grid.png"
+
+#define X_PIECE_FILEPATH "Image/X.png"
+#define O_PIECE_FILEPATH "Image/O.png"
+#define X_WINNING_PIECE_FILEPATH "Image/X Win.png"
+#define O_WINNING_PIECE_FILEPATH "Image/O Win.png"
+
+#define TAG_MAIN_MENU_PLAY_BUTTON 0
+#define TAG_ACHIEVEMENTS_BUTTON 1
+
+#define X_PIECE 8
+#define O_PIECE 0
+#define EMPTY_PIECE -1
+#define PLAYER_PIECE X_PIECE
+#define AI_PIECE O_PIECE
+
+#define STATE_PLAYING 98
+#define STATE_PAUSED 97
+#define STATE_WON 96
+#define STATE_LOSE 95
+#define STATE_PLACING_PIECE 94
+#define STATE_AI_PLAYING 93
+#define STATE_DRAW 92
+
+#define PIECE_FADE_IN_TIME 0.5
+
+#define GAME_OVER_BACKGROUND_FADE_IN_TIME 0.5
+#define PAUSE_BACKGROUND_FADE_TIME 0.5
+
+#define MENU_MOVE_BOUNCE_DURATION 1.5
