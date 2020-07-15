5f04dee5afaa1f86df47af9f378e219c215db532
Minh Tâm <55475305+tamtamxtamtam@users.noreply.github.com>
2020-07-15 17:25:50 +0700

    15/07/20

---
+++ Minh_Tam/FlappyBird/Classes/GameScene.cpp
@@ -0,0 +1,160 @@
+/****************************************************************************
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+#include "GameScene.h"
+#include "Definitions.h"
+#include "Pipe.h"
+#include "GameOverScene.h"
+
+USING_NS_CC;
+
+Scene* GameScene::createScene()
+{
+    auto scene = Scene::createWithPhysics();
+    scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+    scene->getPhysicsWorld()->setGravity(Vect(0, 0));
+
+
+    auto layer = GameScene::create();
+    layer->setPhysisWorld(scene->getPhysicsWorld());
+
+    scene->addChild(layer);
+
+    return scene;
+}
+
+static void problemLoading(const char* filename)
+{
+    printf("Error while loading: %s\n", filename);
+    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+}
+
+// on "init" you need to initialize your instance
+bool GameScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if (!Layer::init())
+    {
+        return false;
+    }
+
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    auto backgroundSprite = Sprite::create("iphonehd/Background.png");
+    backgroundSprite->setPosition(Point(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2));
+    this->addChild(backgroundSprite);
+
+    auto edgeBody = PhysicsBody::createEdgeBox(visibleSize, PHYSICSBODY_MATERIAL_DEFAULT, 3);
+    edgeBody->setCollisionBitmask(OBSTACLE_COLLISION_BITMASK);
+    edgeBody->setContactTestBitmask(false);
+
+    auto edgeNode = Node::create();
+    edgeNode->setPosition(Point(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2));
+    edgeNode->setPhysicsBody(edgeBody);
+
+    this->addChild(edgeNode);
+
+    this->schedule(schedule_selector(GameScene::SpawnPipe), PIPE_SPAWN_FREQUENCY * visibleSize.width);
+
+    bird = new Bird(this);
+
+    auto contactListener = EventListenerPhysicsContact::create();
+    contactListener->onContactBegin = CC_CALLBACK_1(GameScene::onContactBegin, this);
+    Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(contactListener, this);
+
+    auto touchListener = EventListenerTouchOneByOne::create();
+    touchListener->setSwallowTouches(true);
+    touchListener->onTouchBegan = CC_CALLBACK_2(GameScene::onTouchBegan, this);
+    Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(touchListener, this);
+
+    score = 0;
+
+    __String* tempScore = __String::createWithFormat("%i", score);
+    scoreLabel = Label::createWithTTF(tempScore->getCString(), "fonts/Marker Felt.ttf", visibleSize.height * SCORE_FONT_SIZE);
+    scoreLabel->setColor(Color3B::WHITE);
+    scoreLabel->setPosition(Point(origin.x + visibleSize.width / 2, origin.y + visibleSize.height * 0.75));
+    this->addChild(scoreLabel, 10000);
+
+    this->scheduleUpdate();
+
+    return true;
+}
+
+
+void GameScene::menuCloseCallback(Ref* pSender)
+{
+    //Close the cocos2d-x game scene and quit the application
+    Director::getInstance()->end();
+
+    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() as given above,instead trigger a custom event created in RootViewController.mm as below*/
+
+    //EventCustom customEndEvent("game_scene_close_event");
+    //_eventDispatcher->dispatchEvent(&customEndEvent);
+
+
+}
+
+void GameScene::SpawnPipe(float dt) {
+    pipe.SpawnPipe(this);
+}
+
+bool GameScene::onContactBegin(cocos2d::PhysicsContact& contact) {
+    PhysicsBody* a = contact.getShapeA()->getBody();
+    PhysicsBody* b = contact.getShapeB()->getBody();
+
+    if ((BIRD_COLLISION_BITMASK == a->getCollisionBitmask() && OBSTACLE_COLLISION_BITMASK == b->getCollisionBitmask()) || (BIRD_COLLISION_BITMASK == b->getCollisionBitmask() && OBSTACLE_COLLISION_BITMASK == a->getCollisionBitmask())) {
+  
+        //auto scene = GameOverScene::createScene(score);
+
+        //Director::getInstance()->replaceScene(TransitionFade::create(TRANSITION_TIME, scene));
+    }
+    else if ((BIRD_COLLISION_BITMASK == a->getCollisionBitmask() && POINT_COLLISION_BITMASK == b->getCollisionBitmask()) || (BIRD_COLLISION_BITMASK == b->getCollisionBitmask() && POINT_COLLISION_BITMASK == a->getCollisionBitmask())) {
+
+        score++;
+        __String* tempScore = __String::createWithFormat("%i", score);
+        scoreLabel->setString(tempScore->getCString());
+    }
+
+    return true;
+}
+
+bool GameScene::onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event) {
+    bird->Fly();
+
+    this->scheduleOnce(schedule_selector(GameScene::StopFlying), BIRD_FLY_DURATION);
+
+    return true;
+}
+
+void GameScene::StopFlying(float dt) {
+    bird->StopFlying();
+}
+
+void GameScene::update(float dt) {
+    bird->Fall();
+}
+
+
---
+++ Minh_Tam/FlappyBird/Classes/SplashScene.cpp
@@ -0,0 +1,77 @@
+/****************************************************************************
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+#include "SplashScene.h"
+#include "MainMenuScene.h"
+#include "Definitions.h"
+
+USING_NS_CC;
+
+Scene* SplashScene::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+
+    // 'layer' is an autorelease object
+    auto layer = SplashScene::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool SplashScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if (!Scene::init())
+    {
+        return false;
+    }
+
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    this->scheduleOnce(schedule_selector(SplashScene::GoToMainMenuScene), DISPLAY_TIME_SPLASH_SCENE);
+
+    auto backgroundSprite = Sprite::create("Image/tamtam.png");
+    backgroundSprite->setPosition(Point(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+
+    this->addChild(backgroundSprite);
+
+    return true;
+}
+
+void SplashScene::GoToMainMenuScene(float dt)
+{
+    auto scene = MainMenuScene::createScene();
+
+    Director::getInstance()->replaceScene(TransitionFade::create(TRANSITION_TIME, scene));
+}
+
+
+
